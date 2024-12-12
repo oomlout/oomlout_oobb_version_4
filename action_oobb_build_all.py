70eb06704c842da38d54be2f3b8b94bc814bef61
Oom Lout <aaron@oomlout.com>
2024-12-12 16:05:52 +0000

    Update action_oobb_build_all.py

--- action_oobb_build_all.py
+++ action_oobb_build_all.py
@@ -2,14 +2,22 @@ import oobb
 import oobb_base
 import oobb_make_sets
 
+
+
+#just_yaml_files = False
+just_yaml_files = True
+
+
 oobb_make_sets.make_all()
+oobb_base.dump("pickle")
 oobb_base.dump("folder")
 #oobb_base.dump("json")
-oobb_base.dump("pickle")
-oobb_base.build_things(overwrite=False, save_type="all")
-import action_generate_release_3d_printable
-action_generate_release_3d_printable.main()
-import action_generate_release_laser_cut
-action_generate_release_laser_cut.main()
-#input = input("Press Enter to continue...")
+
+if not just_yaml_files:    
+    oobb_base.build_things(overwrite=False, save_type="all")
+    import action_generate_release_3d_printable
+    action_generate_release_3d_printable.main()
+    import action_generate_release_laser_cut
+    action_generate_release_laser_cut.main()
+    #input = input("Press Enter to continue...")
 
